b5a74f74e972f6e56d997ea791b3c06561605c97
Wim van den Heijkant <wim@fortigi.nl>
2020-07-28 16:14:18 +0200

    Demo voorbeeld

--- Examples/Script/Demo.ps1
+++ Examples/Script/Demo.ps1
@@ -9,38 +9,37 @@
 #    -Optional permission for automatic group creation 
 #    Group.Create
 
+################################################################################################################################
+#Load module and get access token
+################################################################################################################################
 
 Import-Module .\ConditionalAccess\ConditionalAccess.psm1 -Force
 
-#$TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-#$ClientId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-#$ClientSecret = "aA_aAaa12A1A1aaAa_a~aAA_1A-AAaaaaaaa" 
+$TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+$ClientId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+$ClientSecret = "aA_aAaa12A1A1aaAa_a~aAA_1A-AAaaaaaaa" 
 
 $AccessToken = Get-AccessToken -ClientId $ClientId -TenantId $TenantId -ClientSecret $ClientSecret
 
-#Get Existing Policies.. in Json Format
-# Policies = Get-ConditionalAccessPolicy -AccessToken $AccessToken -All $true
 
-<#
+################################################################################################################################
+#Example 1; Download existing Conditional Access Policies
+################################################################################################################################
+
+#Get Existing Policies.. in Json Format
+$Policies = Get-ConditionalAccessPolicy -AccessToken $AccessToken
+
+#Export existing policies to JSON files
+Get-ConditionalAccessPolicyFile -AccessToken $AccessToken -Path .\Examples\Policy\Temp
+
+
+################################################################################################################################
+#Example 2; Upload policy
+################################################################################################################################
+
 #Upload a new policy, Be sure to adjust the demo file with the correct displayNames to avoid errors. 
 $File = ".\Examples\Policy\CA-01- All Apps - All Admins - Require MFA.json"
-#New-ConditionalAccessPolicy -accessToken $AccessToken -PolicyFile $File
-
-Get-ConditionalAccessPolicy -accessToken $AccessToken -All $true
-New-ConditionalAccessPolicy -accessToken $AccessToken -PolicyFile $File -CreateMissingGroups $true
-$ca = Get-ConditionalAccessPolicy -accessToken $AccessToken -DisplayName "CA-HBRTEST05 - Azure management - All Users - Require trusted location"
-$ca.conditions.applications.includeApplications
-
-Remove-ConditionalAccessPolicy -Id "9ff72528-ca35-4852-a81f-4125db958e88" -AccessToken $AccessToken
-#>
-
-#Get-ConditionalAccessPolicy -accessToken $AccessToken
-
-Get-ConditionalAccessPolicyFile -AccessToken $AccessToken -Path ".\Examples\Policy\A" #-ConvertGUIDs $False
-
-
-
-
+New-ConditionalAccessPolicy -AccessToken $AccessToken -PolicyFile $File
 
 
 
